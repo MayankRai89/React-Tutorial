288ed1f54f81dd7786348642be17258c917d5c15
Mayank Rai <149506720+MayankRai89@users.noreply.github.com>
2026-02-26 12:31:20 +0530

    update Section1.jsx to ensure minimum height for the container

--- UI Design/src/component/Section1/Section1.jsx
+++ UI Design/src/component/Section1/Section1.jsx
@@ -4,7 +4,7 @@ import Page1Content from "./Page1Content";
 
 const Section1 = ({ users }) => {
   return (
-    <div className="h-screen w-full bg-amber-200">
+    <div className="min-h-screen w-full bg-amber-200">
       <Navbar />
       <Page1Content users={users} />
     </div>
